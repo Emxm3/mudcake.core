9812faa8bde61b74cbc73ef1cded834c1005948c
Pippyyy <the.emx@gmail.com>
2024-05-12 21:47:03 +0800

    Add IHubConnectionFactory to core to allow modules access to hubs

---
+++ Data/Services/IHubConnectionService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudCake.core.Data.Services
+{
+    public interface IHubConnectionService : IDataService
+    {
+        HubConnection Get<THub>() where THub : Hub;
+    }
+}
